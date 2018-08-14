9af767db1645aed486b7e302606338da4f0a1011
Günther Wagner <info@gunibert.de>
2018-08-14 08:02:39 +0200

    fixed header includes in public include header

--- src/alb.h
+++ src/alb.h
@@ -3,12 +3,12 @@
 
 #define __ALB_H_INSIDE__
 
-#include "engine/amsel-cache.h"
-#include "engine/amsel-database.h"
-#include "engine/amsel-sqlite-database.h"
-#include "engine/amsel-engine.h"
-#include "engine/amsel-parser.h"
-#include "engine/amsel-request.h"
-#include "engine/amsel-validator.h"
+#include "amsel-cache.h"
+#include "amsel-database.h"
+#include "amsel-sqlite-database.h"
+#include "amsel-engine.h"
+#include "amsel-parser.h"
+#include "amsel-request.h"
+#include "amsel-validator.h"
 
 #undef  __ALB_H_INSIDE__
